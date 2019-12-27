60b159fb17f7eefcb3c31b8e6e99a4dfd52b8e72
jotyx <jotyx@users.noreply.github.com>
2019-12-27 19:50:32 +0000

    Deploying to master from  b13b21090959594a4471cd8065bfb1f2e168a86c

---
+++ precache-manifest.5e7b1b9b7ed6f326e34ed39dffbd821d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ef6b7a898648c4bb5c8ca78a87cbdf",
+    "url": "/netwex/index.html"
+  },
+  {
+    "revision": "4fe02d84543faded6a5e",
+    "url": "/netwex/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "919f2f4287465569fe80",
+    "url": "/netwex/static/css/main.b0966151.chunk.css"
+  },
+  {
+    "revision": "4fe02d84543faded6a5e",
+    "url": "/netwex/static/js/2.e6f434f3.chunk.js"
+  },
+  {
+    "revision": "919f2f4287465569fe80",
+    "url": "/netwex/static/js/main.74ad5781.chunk.js"
+  },
+  {
+    "revision": "0f35b3d5a916de934f6f",
+    "url": "/netwex/static/js/runtime-main.de051584.js"
+  }
+]);
