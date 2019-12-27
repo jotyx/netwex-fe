a192d1de221efae469b5606254ec5207d0ae4ca1
Adam <>
2019-12-27 20:51:03 +0100

    prepare deployment

--- src/redux/actions/constants.tsx
+++
@@ -1,9 +0,0 @@
-export const ADD_CATEGORY = "ADD_CATEGORY";
-export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
-export const UPDATE_CATEGORY_AMOUNT = "UPDATE_CATEGORY_AMOUNT";
-
-export const SELECT_MONTH = "SELECT_MONTH";
-
-export const UPDATE_DATA = "UPDATE_DATA";
-
-export const SELECT_YEAR = "SELECT_YEAR";
