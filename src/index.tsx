17df0fe3891577b173ab63ae616091505f000d5c
adam.skurla <adam.skurla@gmail.com>
2023-10-26 14:25:34 +0200

    migrate to vitejs

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import {Provider} from 'react-redux';
 import './index.scss';
 import 'bootstrap/dist/css/bootstrap.css';
-import * as serviceWorker from './serviceWorker';
 import store from "./redux/store/configureStore";
 import { Route, Router, withRouter } from 'react-router-dom'
 import Years from "./components/Years";
@@ -20,8 +19,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Router>
   </Provider>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
